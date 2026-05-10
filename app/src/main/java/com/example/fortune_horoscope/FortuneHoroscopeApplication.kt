3c54285d782245c33554a7f35738d2cc3a05b894
hala kablaoui <hala.kablaoui@stu.ibu.edu.ba>
2026-05-11 00:29:56 +0200

    week10

--- app/src/main/java/com/example/fortune_horoscope/FortuneHoroscopeApplication.kt
+++ app/src/main/java/com/example/fortune_horoscope/FortuneHoroscopeApplication.kt
@@ -4,6 +4,6 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class FortunehoroscopeTheme : Application() {
+class FortuneHoroscopeApplication : Application() {
 
 }
